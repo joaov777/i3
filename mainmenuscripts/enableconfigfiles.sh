751085188871b2883d6f64e883ff076174e69f2f
joaov777 <joaov777@gmail.com>
2020-05-08 20:03:51 -0300

    Updating script for config files

--- mainmenuscripts/enableconfigfiles.sh
+++ mainmenuscripts/enableconfigfiles.sh
@@ -9,39 +9,39 @@ clear
 
         echo "Enabling clipboard manager..." ; sleep 1
         sudo cp ~/i3/varied/copyq/copyq.conf ~/.config/copyq/copyq.conf
-        #ln -sf ~/i3/varied/copyq/copyq.conf ~/.config/copyq/copyq.conf
+        sudo ln -sf ~/i3/varied/copyq/copyq.conf ~/.config/copyq/copyq.conf
 
         echo "Enabling screenshot tool..." ; sleep 1
         sudo cp ~/i3/varied/flameshot/flameshot.ini ~/.config/Dharkael/flameshot.ini
-        #ln -sf ~/i3/varied/flameshot/flameshot.ini ~/.config/Dharkael/flameshot.ini
+        sudo n -sf ~/i3/varied/flameshot/flameshot.ini ~/.config/Dharkael/flameshot.ini
 
         echo "Enabling Picom compositor..." ; sleep 1
         sudo cp ~/i3/varied/picom/picom.conf /etc/xdg/picom.conf
-        #ln -sf ~/i3/varied/picom/picom.conf /etc/xdg/picom.conf
+        sudo ln -sf ~/i3/varied/picom/picom.conf /etc/xdg/picom.conf
 
         echo "Enabling py3status bars..." ; sleep 1
         sudo cp ~/i3/varied/py3status/py3statusbars/i3status.conf /etc/i3status.conf
         sudo cp ~/i3/varied/py3status/py3statusbars/i3status2.conf /etc/i3status2.conf
 
-        #ln -sf ~/i3/varied/py3status/py3statusbars/i3status.conf /etc/i3status.conf
-        #ln -sf ~/i3/varied/py3status/py3statusbars/i3status2.conf /etc/i3status2.conf
+        sudo ln -sf ~/i3/varied/py3status/py3statusbars/i3status.conf /etc/i3status.conf
+        sudo ln -sf ~/i3/varied/py3status/py3statusbars/i3status2.conf /etc/i3status2.conf
 
         echo "Enabling rofi launcher..." ; sleep 1
         sudo cp ~/i3/varied/rofi/config ~/.config/rofi/config
-        #ln -sf ~/i3/varied/rofi/config ~/.config/rofi/config
+        sudo ln -sf ~/i3/varied/rofi/config ~/.config/rofi/config
 
         echo "Enabling termite..." ; sleep 1
         sudo cp ~/i3/varied/termite/current_theme /etc/xdg/termite/config
-        #ln -sf ~/i3/varied/termite/current_theme /etc/xdg/termite/config
+        sudo ln -sf ~/i3/varied/termite/current_theme /etc/xdg/termite/config
 
         echo "Enabling zathura..." ; sleep 1
         sudo cp ~/i3/varied/zathura/zathurarc ~/.config/zathura/zathurarc
-        #ln -sf ~/i3/varied/zathura/zathurarc ~/.config/zathura/zathurarc
+        sudo ln -sf ~/i3/varied/zathura/zathurarc ~/.config/zathura/zathurarc
 
         echo "Enabling tap to click..." ; sleep 1
         sudo cp ~/i3/varied/EnableTapToClick_touchpad/90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf
-        #ln -sf ~/i3/varied/EnableTapToClick_touchpad/90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf
+        sudo ln -sf ~/i3/varied/EnableTapToClick_touchpad/90-touchpad.conf /etc/X11/xorg.conf.d/90-touchpad.conf
 
         echo "Enabling i3 config file..." ; sleep 1
         sudo cp ~/i3/varied/i3config/config ~/.config/i3/config
-        #ln -sf ~/i3/varied/i3config/config ~/.config/i3/config
+        sudo ln -sf ~/i3/varied/i3config/config ~/.config/i3/config
